5ce2b00991293ef734d046dc25e0d4811e75ad83
medzumi <pkkudrin@gmail.com>
2022-01-25 22:46:31 +0300

    add dialog singletonse, add keyinput

---
+++ Assets/Scripts/DialogRunnerProvider.cs
@@ -0,0 +1,10 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+using Yarn.Unity;
+
+public class DialogRunnerProvider : Singletone<DialogRunnerProvider>
+{
+    [SerializeField] private DialogueRunner _dialogueRunner;
+
+    public DialogueRunner DialogueRunner => _dialogueRunner;
+}
---
+++ Assets/Scripts/NPCInputHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class NPCInputHelper : MonoBehaviour
+    {
+        [SerializeField] private KeyCode _inputKeyCode = KeyCode.E;
+        [SerializeField] private Text _text;
+
+        public UnityEvent OnKeyCode;
+
+        private void OnEnable()
+        {
+            if (_text)
+            {
+                _text.text = _inputKeyCode.ToString();
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(_inputKeyCode))
+            {
+                OnKeyCode?.Invoke();
+            }
+        }
+    }
+}
