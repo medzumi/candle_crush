6dcd083f3febd1a2febb9d72193426f7470664c9
GreenBlackSky <greenblacksky@gmail.com>
2022-01-29 01:06:10 +0300

    dialogues for cat and bear

--- Assets/Scripts/NPCDialog.cs
+++ Assets/Scripts/NPCDialog.cs
@@ -8,19 +8,19 @@ public class NPCDialog : MonoBehaviour
 {
     public UnityEvent OnComplete;
     
-    [SerializeField] private string NPCName;
+    [SerializeField] private string StartNode;
 
     private DialogueRunner _dialogueRunner => DialogRunnerProvider.Instance.DialogueRunner;
 
     public void ShowDialog()
     {
-        _dialogueRunner.StartDialogue(NPCName);
+        _dialogueRunner.StartDialogue(StartNode);
         _dialogueRunner.onNodeComplete.AddListener(NodeCompleteHandler);
     }
 
     private void NodeCompleteHandler(string arg0)
     {
-        if (string.Equals(NPCName, arg0))
+        if (string.Equals(StartNode, arg0))
         {
             OnComplete.Invoke();
         }
