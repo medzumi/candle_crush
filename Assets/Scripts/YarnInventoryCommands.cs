08b6d3e8620b661d3c8550d006fe14260a8e76f7
medzumi <pkkudrin@gmail.com>
2022-01-29 16:42:26 +0300

    yarn dialogues

--- Assets/Scripts/YarnInventoryCommands.cs
+++ Assets/Scripts/YarnInventoryCommands.cs
@@ -5,28 +5,28 @@ namespace DefaultNamespace
 {
     public class YarnInventoryCommands : MonoBehaviour
     {
-        private InventorySystem InventorySystem => Singletone<InventorySystem>.Instance;
+        private static InventorySystem InventorySystem => Singletone<InventorySystem>.Instance;
 
         [YarnCommand("AddItem")]
-        public void AddItem(string id)
+        public static void AddItem(string id)
         {
             InventorySystem.AddItem(new UniPair<string, int>(){Key = id, Value = 1});
         }
 
         [YarnCommand("AddItemWithCount")]
-        public void AddItemWithCount(string id, int count)
+        public static void AddItemWithCount(string id, int count)
         {
             InventorySystem.AddItem(new UniPair<string, int>(){Key = id, Value = count});
         }
 
         [YarnCommand("RemoveItem")]
-        public void RemoveItem(string id)
+        public static void RemoveItem(string id)
         {
             InventorySystem.RemoveItem((new UniPair<string, int> {Key = id}));
         }
         
         [YarnCommand("RemoveItemWithCount")]
-        public void RemoveItemWithCount(string id, int count)
+        public static void RemoveItemWithCount(string id, int count)
         {
             InventorySystem.RemoveItem((new UniPair<string, int> {Key = id}));
         }
